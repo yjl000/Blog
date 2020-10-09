5b6917f96414a2a562869ffadd515f28c2f95f72
kenyang <kenyang@lexin.com>
2020-10-09 18:07:25 +0800

    react组件提升以及骨架屏

--- react/tic-tac-toe/src/index.js
+++ react/tic-tac-toe/src/index.js
@@ -2,16 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './view/clock/clock'
 import Game from './view/game/index';
-import Reservation from './view/form/form'
+import Reservation from './view/form/form';
+import Boil from './view/boil/boil';
+import { Facebook } from "react-content-loader";
 
-function App(props) {
-  return (
-    <div>
-      <Game />
-      <Clock />
-      <Reservation />
-    </div>
-  )
+const MyFacebookLoader = () => <Facebook />
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {isMounted: false}
+  }
+  
+  render() {
+    let loader;
+    if (!this.state.isMounted) {
+      loader = <div>
+                  <MyFacebookLoader />
+                </div>
+    } else {
+      loader = <div>
+                <Game />
+                <Clock />
+                <Reservation />
+                <Boil />
+              </div>
+    }
+    setTimeout(() => {
+      this.setState({
+        isMounted: true
+      })
+    }, 3000);
+    return (
+      <div>
+        {loader}
+      </div>
+      
+    )
+  }
+  
 }
 
 
--- react/tic-tac-toe/src/view/form/form.js
+++ react/tic-tac-toe/src/view/form/form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 class Reservation extends React.Component {
   constructor(props) {
