9824b3dcad36d3d57c40ff6f856cc124ed3b0dd3
“yjl000” <623139771@qq.com>
2021-04-01 09:27:11 +0800

    完成列表页面样式重构以及第一个自定义函数

---
+++ vue/vue3/vue3_ts/kenblog/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+    <global-header :user="user"></global-header>
+    <column-list :list="list"></column-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+const user: UserProps = {
+  isLogin: true,
+  name: 'ken'
+}
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1的专栏，有一段非常有意思的简介',
+    avatar: 'https://www.baidu.com/img/flexible/logo/pc/result@2.png'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是test2的专栏，有一段非常有意思的简介',
+    avatar: 'https://www.baidu.com/img/flexible/logo/pc/result@2.png'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这是test1的专栏，有一段非常有意思的简介',
+    avatar: ''
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这是test2的专栏，有一段非常有意思的简介',
+    avatar: 'https://www.baidu.com/img/flexible/logo/pc/result@2.png'
+  }
+]
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList,
+    GlobalHeader
+  },
+  setup () {
+    return {
+      list: testData,
+      user
+    }
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue3/vue3_ts/kenblog/src/components/ColumnList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img
+            width="100"
+            :src="column.avatar"
+            :alt="column.title"
+            class="rounded-circle border border-light w-25 my-3"
+          />
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-left">{{ column.description }}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { computed, defineComponent, PropType } from 'vue'
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar?: string;
+  description: string;
+}
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  },
+  setup (props) {
+    const columnList = computed(() => {
+      return props.list.map((column) => {
+        if (!column.avatar) {
+          column.avatar = require('@/assets/logo.png')
+        }
+        return column
+      })
+    })
+    return {
+      columnList
+    }
+  }
+})
+</script>
