5d77a91989fb63a3ce49637f47a6a04bd5075d33
“yjl000” <623139771@qq.com>
2025-07-31 20:51:57 +0800

    feat: create hardhat content

---
+++ DApps/hardhatContent/hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+    }
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
+
+export default config;
