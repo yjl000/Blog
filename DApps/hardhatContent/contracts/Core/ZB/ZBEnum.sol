43023e3bd11bb29958005d38e105ae5b7ddf7d9e
“yjl000” <623139771@qq.com>
2025-08-04 21:15:47 +0800

    feat: add game mode code

---
+++ DApps/hardhatContent/contracts/Core/ZB/ZBEnum.sol
@@ -0,0 +1,59 @@
+//THIS FILE IS AUTO GENERATED IN THE GAMECHAIN REPO
+//TO MAKE CHANGES YOU NEED TO EDIT THE GAMECHAIN REPO
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+library ZBEnum {
+
+    enum AbilityActivityType
+    {
+            PASSIVE,
+            ACTIVE
+    }
+
+    enum AbilityCallType
+    {
+            TURN,
+            ENTRY,
+            END,
+            ATTACK,
+            DEATH,
+            PERMANENT,
+            GOT_DAMAGE,
+            AT_DEFENCE,
+            IN_HAND
+    }
+
+    enum PlayerAction
+    {
+            StartGame,
+            EndTurn,
+            DrawCardPlayer,
+            PlayCard,
+            CardAttack,
+            UseCardAbility
+    }
+
+    enum GameStateChangeAction
+    {
+            None,
+            SetPlayerDefense,
+            SetPlayerCurrentGoo,
+            SetPlayerGooVials,
+            SetPlayerCardsInDeck,
+            SetPlayerCardsInHand,
+            SetPlayerInitialCardsInHandCount,
+            SetPlayerMaxCardsInPlay,
+            SetPlayerMaxCardsInHand,
+            SetPlayerMaxGooVials,
+            SetPlayerTurnTime
+    }
+
+    enum CustomUiElement
+    {
+            None,
+            Label,
+            Button
+    }
+
+}
---
+++ DApps/hardhatContent/contracts/Core/exampleGame.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "./ZB/ZBGameMode.sol";
+
+contract ExampleGame is ZBGameMode {
+  function beforeMatchStart(bytes memory serializedGameState) external {
+
+  }
+}
