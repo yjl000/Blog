50000f27135942fa881925b862dcde8f240b4b74
yjl000 <623139771@qq.com>
2025-09-19 23:40:19 +0800

    create import contract

---
+++ DApps/updraftLearn/contracts/StorageFactory.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { SimpleStorage } from "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage public simpleStorage;
+    function createSimpleStorageContract() public  {
+        simpleStorage = new SimpleStorage();
+    }
+}
