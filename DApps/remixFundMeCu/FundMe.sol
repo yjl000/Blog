4844437a63ee65e33868b9a0dab85e9f8fd22ad6
yjl000 <623139771@qq.com>
2025-10-13 23:39:26 +0800

    learn interface and ChainLink AggregatorV3Interface

--- DApps/remixFundMeCu/FundMe.sol
+++ DApps/remixFundMeCu/FundMe.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
 contract FundMe {
     // send funds into our contract 
     uint256 public myValue = 1;
@@ -8,6 +12,25 @@ contract FundMe {
         myValue = myValue + 1;
         require(msg.value > 1e18, "didn't send enough ETH");
     }
+
+    /**
+    * Network: Sepolia
+    * Data Feed: ETH/USD
+    * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    */
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function decimals() external view returns (uint8) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.decimals();
+    }
+
+
+
 
     // owner can withdraw funds
     // function withdraw() public  {}
